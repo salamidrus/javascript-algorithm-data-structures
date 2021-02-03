1b51401bdc4c5912f2293235fa3f4fedff78467a
salamidrus <idrussalam95gmail.com>
2021-02-03 13:11:44 +0700

    Add Description for all tasks

--- Problem Solving Patterns/Frequency Counter/same_naive_solution.js
+++ Problem Solving Patterns/Frequency Counter/same_naive_solution.js
@@ -1,17 +1,20 @@
-function same(arr1, arr2){
-    if(arr1.length !== arr2.length){
-        return false;
+// write a function to check if arr2 contains the arr1 quadratic number
+// the amount of numbers should be same
+
+function same(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    let correctIndex = arr2.indexOf(arr1[i] ** 2);
+    if (correctIndex === -1) {
+      return false;
     }
-    for(let i = 0; i < arr1.length; i++){
-        let correctIndex = arr2.indexOf(arr1[i] ** 2)
-        if(correctIndex === -1) {
-            return false;
-        }
-        console.log(arr2);
-        arr2.splice(correctIndex,1)
-    }
-    return true;
+    console.log(arr2);
+    arr2.splice(correctIndex, 1);
+  }
+  return true;
 }
 
-same([1,2,3,2], [9,1,4,4])
-
+same([1, 2, 3, 2], [9, 1, 4, 4]);
+same([1, 2, 2, 2], [9, 1, 4, 4]); // false, different amount of numbers
--- Problem Solving Patterns/Frequency Counter/same_refactored_solution.js
+++ Problem Solving Patterns/Frequency Counter/same_refactored_solution.js
@@ -1,26 +1,30 @@
-function same(arr1, arr2){
-    if(arr1.length !== arr2.length){
-        return false;
+// write a function to check if arr2 contains the arr1 quadratic number
+// the amount of numbers should be same
+
+function same(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  let frequencyCounter1 = {};
+  let frequencyCounter2 = {};
+  for (let val of arr1) {
+    frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1;
+  }
+  for (let val of arr2) {
+    frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1;
+  }
+  console.log(frequencyCounter1);
+  console.log(frequencyCounter2);
+  for (let key in frequencyCounter1) {
+    if (!(key ** 2 in frequencyCounter2)) {
+      return false;
     }
-    let frequencyCounter1 = {}
-    let frequencyCounter2 = {}
-    for(let val of arr1){
-        frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1
+    if (frequencyCounter2[key ** 2] !== frequencyCounter1[key]) {
+      return false;
     }
-    for(let val of arr2){
-        frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1        
-    }
-    console.log(frequencyCounter1);
-    console.log(frequencyCounter2);
-    for(let key in frequencyCounter1){
-        if(!(key ** 2 in frequencyCounter2)){
-            return false
-        }
-        if(frequencyCounter2[key ** 2] !== frequencyCounter1[key]){
-            return false
-        }
-    }
-    return true
+  }
+  return true;
 }
 
-same([1,2,3,2,5], [9,1,4,4,11])
+same([1, 2, 3, 2, 5], [9, 1, 4, 4, 11]);
+same([1, 2, 2, 2], [9, 1, 4, 4]); // false, different amount of numbers
