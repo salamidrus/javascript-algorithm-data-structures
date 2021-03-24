73b223f5d09f754d09e9ad55f58dc232b1a99810
salamidrus <idrussalam95gmail.com>
2021-03-24 10:40:21 +0700

    Add Merge Sort

--- Sort Algorithm/Bubble Sort/bubble_unoptimized.js
+++ Sort Algorithm/Bubble Sort/bubble_unoptimized.js
@@ -1,12 +1,12 @@
 // UNOPTIMIZED VERSION OF BUBBLE SORT
-function bubbleSort(arr){
-  for(var i = arr.length; i > 0; i--){
-    for(var j = 0; j < i - 1; j++){
-      console.log(arr, arr[j], arr[j+1]);
-      if(arr[j] > arr[j+1]){
+function bubbleSort(arr) {
+  for (var i = arr.length; i > 0; i--) {
+    for (var j = 0; j < i - 1; j++) {
+      console.log(arr, arr[j], arr[j + 1], i);
+      if (arr[j] > arr[j + 1]) {
         var temp = arr[j];
-        arr[j] = arr[j+1];
-        arr[j+1] = temp;         
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
       }
     }
   }
@@ -29,4 +29,4 @@ function bubbleSort(arr) {
   return arr;
 }
 
-bubbleSort([8,1,2,3,4,5,6,7]);
+bubbleSort([8, 1, 2, 3, 4, 5, 6, 7]);
